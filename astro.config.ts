d05b4abd22d91ff3ffe2bb4fa1cb154d7dc4d2a1
Jacob S <jsimeone0105@gmail.com>
2025-04-28 09:05:19 -0500

    Add deployment action

--- astro.config.ts
+++ astro.config.ts
@@ -24,6 +24,10 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 export default defineConfig({
   output: 'static',
 
+  // For Github pages
+  site: 'https://litemage.github.io',
+  base: 'bu-vip-website',
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
